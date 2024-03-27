77f64abe24cdc2f36db64ee32bb9bc809e4eead2
MaoCarmona <89806416+MaoCarmona@users.noreply.github.com>
2024-03-26 22:43:22 -0500

    Optimize code

--- src/movies/infrastrusture/persistence/movies.json.repository.ts
+++ src/movies/infrastrusture/persistence/movies.json.repository.ts
@@ -16,24 +16,17 @@ import { paginatedResponse } from "@app/shared/domain/utils/paginated.utill";
 import * as _ from 'lodash';
 
 @Injectable()
-export class MoviesJsonRepository
-  extends JsonRepository<MovieModel>
-  implements IMoviesRepository
-{
+export class MoviesJsonRepository extends JsonRepository<MovieModel> implements IMoviesRepository {
+  protected get entityName(): string {
+   return '';
+  }
   public constructor(protected readonly jsonDataSource: JsonDataSource<MovieModel>) {
     super(jsonDataSource);
-  }
-  protected readonly entityName = "";
-
-  public async findByTitle(title: string): Promise<MovieModel | null> {
-    const allMovies = await this.getAll();
-    return allMovies.find((movie) => movie.title === title) || null;
   }
 
   public async findAll(paginated: findAllQuery): Promise<MovieModel[]> {
     const { categorizeBy } = paginated;
-    const selectedFunction =
-      this.categoryFunctions[categorizeBy] || this.categoryFunctions["default"];
+    const selectedFunction = this.categoryFunctions[categorizeBy] || this.categoryFunctions["default"];
     return await selectedFunction(paginated);
   }
 
@@ -46,77 +39,60 @@ export class MoviesJsonRepository
     default: this.getAllPaginated.bind(this),
   };
 
-  private async getAllPaginated(
-    paginated: findAllQuery
-  ): Promise<MovieModel[]> {
+  private async getAllPaginated(paginated: findAllQuery): Promise<MovieModel[]> {
     const allMovies = await this.getAll();
     return paginatedResponse<MovieModel>(allMovies, paginated);
   }
 
+  public async findByTitle(title: string): Promise<MovieModel | null> {
+    const allMovies = await this.getAll();
+    return allMovies.find(movie => movie.title === title) || null;
+  }
+
   public async findByActor(paginated: findAllQuery): Promise<MovieModel[]> {
     const allMovies = await this.getAll();
-    const filteredMovies: MovieModel[] = allMovies.filter((movie) =>
-      movie.actors.includes(paginated.actor)
-    );
+    const filteredMovies: MovieModel[] = allMovies.filter(movie => movie.actors.includes(paginated.actor));
     return plainToInstance(
       MoviesByActorResponseDto,
       paginatedResponse<MovieModel>(filteredMovies, paginated)
     );
   }
 
-  // Sorting By popularity
-  public async sortByPopularity(
-    paginated: findAllQuery
-  ): Promise<MovieModel[]> {
-    const { order } = paginated;
+  public async sortByPopularity(paginated: findAllQuery): Promise<MovieModel[]> {
     const allMovies = await this.getAll();
-    const sortedMovies = _.orderBy(allMovies, movie => movie.popularity =this.calculatePopularity(movie), order.toLowerCase());
+    const sortedMovies = _.orderBy(allMovies, movie => movie.popularity = this.calculatePopularity(movie), paginated.order.toLowerCase());
     return plainToInstance(
       MoviesByPopularityResponseDto,
       paginatedResponse<MovieModel>(sortedMovies, paginated)
     );
   }
+
   private calculatePopularity(movie: MovieModel): number {
     const ratingWeight = 0.5;
     const totalRatingsWeight = 0.3;
     const viewerCountWeight = 0.2;
     const averageRating = this.calculateAverageRating(movie.ratings);
-    const popularity =
-      averageRating * ratingWeight +
-      movie.ratings.length * totalRatingsWeight +
-      movie.viewerCount * viewerCountWeight;
-    return popularity;
+    return averageRating * ratingWeight + movie.ratings.length * totalRatingsWeight + movie.viewerCount * viewerCountWeight;
   }
+
   private calculateAverageRating(ratings: number[]): number {
     if (ratings.length === 0) {
       return 0;
     }
     const sum = ratings.reduce((total, rating) => total + rating, 0);
-    const average = sum / ratings.length;
-    return average;
+    return sum / ratings.length;
   }
-  //End Sorting By popularity
 
-  //Searching by similarity
-  public async findSimilarMovies(
-    paginated: findAllQuery
-  ): Promise<MovieModel[]> {
-    const { title, threshold,order } = paginated;
+  public async findSimilarMovies(paginated: findAllQuery): Promise<MovieModel[]> {
+    const { title, threshold, order } = paginated;
     const allMovies: MovieModel[] = await this.getAll();
-    const movie: MovieModel = allMovies.find((movie) => movie.title === title);
-    const similarMoviesWithScores = allMovies.map((otherMovie) => ({
+    const movie: MovieModel = allMovies.find(movie => movie.title === title);
+    const similarMoviesWithScores = allMovies.map(otherMovie => ({
       movie: otherMovie,
       similarity: this.calculateSimilarityScore(movie, otherMovie),
     }));
-    // Filter similar movies based on the similarity threshold
-    const similarMovies = similarMoviesWithScores.filter(
-      (item) => item.similarity >= threshold
-    );
-
-    const mappedMovies: MovieModel[] = similarMovies.map((item) => ({
-      ...item.movie,
-      similarity: item.similarity,
-    }));
+    const similarMovies = similarMoviesWithScores.filter(item => item.similarity >= threshold);
+    const mappedMovies: MovieModel[] = similarMovies.map(item => ({ ...item.movie, similarity: item.similarity }));
     const sortedMovies = _.orderBy(mappedMovies, 'similarity', order.toLowerCase());
     return plainToInstance(
       MoviesBySimilarityResponseDto,
@@ -124,77 +100,55 @@ export class MoviesJsonRepository
     );
   }
 
-  private calculateSimilarityScore(
-    movieA: MovieModel,
-    movieB: MovieModel
-  ): number {
-    const genreWeight = 0.5; // Greater weight in gender
+  private calculateSimilarityScore(movieA: MovieModel, movieB: MovieModel): number {
+    const genreWeight = 0.5;
     const ratingWeight = 0.3;
     const castWeight = 0.2;
-
-    // Calculate weighted similarity score
-    const genreScore = this.calculateGenreSimilarity(
-      movieA.genres,
-      movieB.genres
-    );
-    const ratingScore = Math.abs(
-      this.calculateAverageRating(movieA.ratings) -
-        this.calculateAverageRating(movieB.ratings)
-    );
-    const castScore = this.calculateCastSimilarity(
-      movieA.actors,
-      movieB.actors
-    );
-
-    const weightedScore =
-      genreScore * genreWeight +
-      ratingScore * ratingWeight +
-      castScore * castWeight;
-    return weightedScore;
+    const genreScore = this.calculateGenreSimilarity(movieA.genres, movieB.genres);
+    const ratingScore = Math.abs(this.calculateAverageRating(movieA.ratings) - this.calculateAverageRating(movieB.ratings));
+    const castScore = this.calculateCastSimilarity(movieA.actors, movieB.actors);
+    return genreScore * genreWeight + ratingScore * ratingWeight + castScore * castWeight;
   }
 
-  private calculateGenreSimilarity(
-    genresA: string[],
-    genresB: string[]
-  ): number {
-    // Calculate gender similarity as the number of shared genders.
-    const sharedGenres = genresA.filter((genre) => genresB.includes(genre));
+  private calculateGenreSimilarity(genresA: string[], genresB: string[]): number {
+    const sharedGenres = genresA.filter(genre => genresB.includes(genre));
     return sharedGenres.length / Math.max(genresA.length, genresB.length);
   }
 
   private calculateCastSimilarity(castA: string[], castB: string[]): number {
-    // Calculate the cast similarity as the number of shared actors.
-    const sharedCast = castA.filter((actor) => castB.includes(actor));
+    const sharedCast = castA.filter(actor => castB.includes(actor));
     return sharedCast.length / Math.max(castA.length, castB.length);
   }
-  // End of searching by Similarity
 
   public async sortByDuration(paginated: findAllQuery): Promise<MovieModel[]> {
+    return this.sortByAttribute(paginated, 'duration',MoviesByDurationResponseDto);
+  }
+
+  public async sortByYear(paginated: findAllQuery): Promise<MovieModel[]> {
+    return this.sortByAttribute(paginated, 'releaseDate', MoviesByYearResponseDto);
+  }
+
+  private async sortByAttribute(paginated: findAllQuery, attribute: string, T): Promise<MovieModel[]> {
     const allMovies = await this.getAll();
-    const { order } = paginated;
-    const sortedMovies = _.orderBy(allMovies, movie => this.convertDurationToMinutes(movie.duration), order.toLowerCase());
-
+    const sortedMovies = _.orderBy(allMovies, movie => this.extractAttributeValue(movie, attribute), paginated.order.toLowerCase());
     return plainToInstance(
-        MoviesByDurationResponseDto,
-        paginatedResponse<MovieModel>(sortedMovies, paginated)
-    );
-}
-
-public async sortByYear(paginated: findAllQuery): Promise<MovieModel[]> {
-  const allMovies = await this.getAll();
-  const { order } = paginated;
-  const getReleaseYear = (movie: MovieModel) => {
-      return parseInt(movie.year);
-  };
-  const sortedMovies = _.orderBy(allMovies, getReleaseYear, order.toLowerCase());
-  return plainToInstance(
-      MoviesByYearResponseDto,
+      T,
       paginatedResponse<MovieModel>(sortedMovies, paginated)
-  );
-}
+    );
+  }
+
+  private extractAttributeValue(movie: MovieModel, attribute: string): any {
+    if (attribute === 'duration') {
+      return this.convertDurationToMinutes(movie.duration);
+    } else if (attribute === 'releaseDate') {
+      return new Date(movie.releaseDate).getFullYear();
+    } else {
+      throw new Error(`Atributo no válido: ${attribute}`);
+    }
+  }
 
   private convertDurationToMinutes(duration: string): number {
-    const durationRegex = /PT(?:(\d+)H)?(?:(\d+)M)?/; // Regular expression to extract hours and minutes
+    const durationRegex = /PT(?:(\d+)H)?(?:(\d+)M)?/;
     const matches = duration.match(durationRegex);
 
     if (!matches) {
@@ -207,3 +161,4 @@ public async sortByYear(paginated: findAllQuery): Promise<MovieModel[]> {
     return hours * 60 + minutes;
   }
 }
+
